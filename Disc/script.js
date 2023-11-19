b6488fb55631fcb7cddec273932a58aa93a434be
Adekoye Adewale <adekoyeadewale@gmail.com>
2023-11-19 15:55:45 +0100

    update
    Disc style custom cursor
    Scroll animation using CSS only

---
+++ Disc/script.js
@@ -0,0 +1,25 @@
+var cursor = document.querySelector('.disc__cursor');
+var link = document.querySelectorAll('a');
+
+document.addEventListener('mousemove', function(e){
+    var x = e.clientX;
+    var y = e.clientY;
+    cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+});
+
+document.addEventListener('mousedown', function(){
+cursor.classList.add('cursor__active');
+});
+
+document.addEventListener('mouseup', function(){
+cursor.classList.remove('cursor__active');
+});
+
+link.forEach(item => {
+    item.addEventListener('mouseover', () => {
+      cursor.classList.add('link__hover');
+    });
+    item.addEventListener('mouseleave', () => {
+      cursor.classList.remove('link__hover');
+    });
+})
