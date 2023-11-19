59b8f3f75feb1a7813d944f6dbb5ad26a5e1dc39
Adekoye Adewale <adekoyeadewale@gmail.com>
2023-11-19 13:22:55 +0100

    update
    Added entrance scene for the images on scroll feature

--- Donut/script.js
+++ Donut/script.js
@@ -1,6 +1,7 @@
 var cursor = document.querySelector('.cursor__donut');
 var cursordot = document.querySelector('.cursor__dot');
 var link = document.querySelectorAll('a');
+var img = document.querySelectorAll('img');
 
 document.addEventListener('mousemove', function(e){
   var x = e.clientX;
@@ -33,3 +34,38 @@ link.forEach(item => {
     cursordot.classList.remove('cursor__dot__link__hover');
   });
 })
+
+img.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('image__hover');
+    cursordot.classList.add('cursor__dot__image__hover');
+  });
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('image__hover');
+    cursordot.classList.remove('cursor__dot__image__hover');
+  });
+})
+
+// Entrance scene on viewport 
+const images = document.querySelectorAll('.in__view');
+
+const options = {
+  threshold: 0.3
+};
+
+const handleIntersection = (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('in-viewport');
+    } else {
+      entry.target.classList.remove('in-viewport');
+    }
+  });
+};
+
+const observers = [];
+images.forEach(image => {
+  const observer = new IntersectionObserver(handleIntersection, options);
+  observer.observe(image);
+  observers.push(observer);
+});
